e16c3887824eb7b34cae8fc5c2619ed1a32709e8
Harshakabari <harshakabariedu@gmail.com>
2024-03-02 19:32:56 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var typeData = new Typed(".role", {
+        strings: [
+          "MERN-Stack Developer",
+          "Web Developer",
+          "Backend Developer",
+          "Frontend Developer",
+        ],
+        loop: true,
+        typeSpeed: 100,
+        backSpeed: 80,
+        backDelay: 1000,
+      });
