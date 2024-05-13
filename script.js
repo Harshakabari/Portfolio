0dca6d11568e4a5f10279a4c5f25f3796277bb81
Harshakabari <harshakabariedu@gmail.com>
2024-05-13 11:46:37 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "MERN-Stack Developer",
+      "Web Developer",
+      "Backend Developer",
+      "Frontend Developer",
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 80,
+    backDelay: 1000,
+  });
+
+
+  var swiper = new Swiper('.blog-slider', {
+    spaceBetween: 30,
+    effect: 'fade',
+    loop: true,
+    mousewheel: {
+      invert: false,
+    },
+    // autoHeight: true,
+    pagination: {
+      el: '.blog-slider__pagination',
+      clickable: true,
+    }
+  });
